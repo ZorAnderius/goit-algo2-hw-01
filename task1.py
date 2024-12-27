a6e10ba4c4f58c291b1a9bac09dc75d65b413bdf
ZorAnderius <gabriel.zorander@gmail.com>
2024-12-27 14:59:58 +0000

    add task1

--- task1.py
+++ task1.py
@@ -1,5 +1,17 @@
-def run():
-    pass
+def min_max(arr: list[int]) -> tuple[int, int]:
+    if len(arr) == 1:
+        return arr[0], arr[0]
+
+    if len(arr) == 2:
+        return min(arr[0], arr[1]), max(arr[0], arr[1])
+
+    mid = len(arr) // 2
+    left = min_max(arr[:mid])
+    right = min_max(arr[mid:])
+
+    return min(left[0], right[0]), max(left[1], right[1])
 
 if __name__ == '__main__':
-    run()
+    array = [38, -5, 12, 66, 11, -4, -20, 10, -80]
+    min_val, max_val = min_max(array)
+    print(f"Min: {min_val}, Max: {max_val}")
